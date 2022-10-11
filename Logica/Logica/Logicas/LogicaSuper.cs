28100b7342c85e6f014f1f323d298e493cb21879
Franco44305 <Franco44305@gmail.com>
2022-10-11 18:57:02 -0300

    Lógica del Super

--- Logica/Logica/Logicas/LogicaSuper.cs
+++ Logica/Logica/Logicas/LogicaSuper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Logica.Contratos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,32 @@ namespace Logica.Logicas
 {
     public class LogicaSuper : Archivo
     {
-        List<Contratos.Super> Supermercado { get; set; }
+        private static List<Producto> Supermercado { get; set; }
+        bool Permitir;
+
+        public static List<Producto> ObtenerListaSupermercado()
+        {
+            //Lista supermercado se iguala al resultado de la lectura del JSON 
+
+            return Supermercado;
+        }
+        public bool CargarLista(Producto producto) //Este metodo va a recibir los productos que se vayan agotando en despensa
+        {
+            Supermercado.Add(producto);
+            return true;
+        }
+        public bool VaciarLista() //Método para vaciar la lista del super
+        {
+            if (Permitir == true) //Si ya se compro
+            {
+                Supermercado.Clear(); //Vacio la lista
+                return true;
+            }
+            else
+            {
+                return false; //En caso de que no se pueda no me dejaria
+            }
+
+        }
     }
 }
