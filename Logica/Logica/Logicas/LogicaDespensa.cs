d393bec1e4c9d4810bbe88922a84bf38c2aa988a
MateoTomas0912 <99299338+MateoTomas0912@users.noreply.github.com>
2022-10-15 18:40:14 -0300

    Logica Despensa

--- Logica/Logica/Logicas/LogicaDespensa.cs
+++ Logica/Logica/Logicas/LogicaDespensa.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Logica.Contratos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,45 @@ namespace Logica.Logicas
 {
     public class LogicaDespensa : Archivo 
     {
-        List<Contratos.Despensa> Despensa { get; set; }
+        private List<Producto> Despensa { get; set; }
 
-        //Se iguala la lista de despensa a la lectura del JSON
+        Archivo archivo = new Archivo();
+
+        public void LeerDespensa()
+        {
+            Despensa = archivo.Lectura();
+        }
+
+        public void EscribirDespensa()
+        {
+            string pathDespensa = "despensa.txt";
+            archivo.Escritura(pathDespensa, Despensa);
+        }
+
+        public void EliminarProducto(Producto producto)
+        {
+            Despensa.Remove(producto);
+        }
+
+        public void CrearActualizarProducto(Producto producto)
+        {
+            if (!Despensa.Contains(producto)) // crear nuevo
+            {
+                //generar codigo
+                Despensa.Add(producto);
+            }
+            else
+            {
+                //edito el usuario que tenga el codigo
+                foreach (var item in Despensa)
+                {
+                    if (item.Codigo == producto.Codigo) //este quiero editar!
+                    {
+                        item.Nombre = producto.Nombre;
+                        item.Precio = producto.Precio;
+                    }
+                }
+            }
+        }
     }
 }
