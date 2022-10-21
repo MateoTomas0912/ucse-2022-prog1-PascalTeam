d29cd70aa2247859f0c6f432b676c8c36f3dc66f
MateoTomas0912 <99299338+MateoTomas0912@users.noreply.github.com>
2022-10-21 17:02:46 -0300

    Lectura fixed

--- Logica/Logica/Logicas/Archivo.cs
+++ Logica/Logica/Logicas/Archivo.cs
@@ -52,122 +52,119 @@ namespace Logica.Logicas
             }
 
             //Carne
-            List<Carne> carnes = new List<Carne>();
-            using (StreamReader reader = new StreamReader(pathDirectorio + "carne.txt"))
+            if (File.Exists(pathDirectorio + "carne.txt"))
             {
-                if (!File.Exists(pathDirectorio + "carne.txt"))
+                List<Carne> carnes = new List<Carne>();
+                using (StreamReader reader = new StreamReader(pathDirectorio + "carne.txt"))
                 {
-                    List<Producto> productosVacios = new List<Producto>();
-                    return productosVacios;
-                }
-                string carne = reader.ReadToEnd();
-                carnes = JsonConvert.DeserializeObject<List<Carne>>(carne);
-                foreach (var producto in carnes)
-                {
-                    productos.Add(producto);
+                    string carne = reader.ReadToEnd();
+                    carnes = JsonConvert.DeserializeObject<List<Carne>>(carne);
+                    foreach (var producto in carnes)
+                    {
+                        productos.Add(producto);
+                    }
                 }
             }
 
+
             //Bebida
-            List<Bebida> bebidas = new List<Bebida>();
-            using (StreamReader reader = new StreamReader(pathDirectorio + "bebida.txt"))
+            if (File.Exists(pathDirectorio + "bebida.txt"))
             {
-                if (!File.Exists(pathDirectorio + "bebida.txt"))
+                List<Bebida> bebidas = new List<Bebida>();
+                using (StreamReader reader = new StreamReader(pathDirectorio + "bebida.txt"))
                 {
-                    List<Producto> productosVacios = new List<Producto>();
-                    return productosVacios;
-                }
-                string bebida = reader.ReadToEnd();
-                bebidas = JsonConvert.DeserializeObject<List<Bebida>>(bebida);
-                foreach (var producto in bebidas)
-                {
-                    productos.Add(producto);
+                    string bebida = reader.ReadToEnd();
+                    bebidas = JsonConvert.DeserializeObject<List<Bebida>>(bebida);
+                    foreach (var producto in bebidas)
+                    {
+                        productos.Add(producto);
+                    }
                 }
             }
 
             //Fruta
-            List<Fruta> frutas = new List<Fruta>();
-            using (StreamReader reader = new StreamReader(pathDirectorio + "fruta.txt"))
+            if (File.Exists(pathDirectorio + "fruta.txt"))
             {
-                if (!File.Exists(pathDirectorio + "fruta.txt"))
+                List<Fruta> frutas = new List<Fruta>();
+                using (StreamReader reader = new StreamReader(pathDirectorio + "fruta.txt"))
                 {
-                    List<Producto> productosVacios = new List<Producto>();
-                    return productosVacios;
-                }
-                string fruta = reader.ReadToEnd();
-                frutas = JsonConvert.DeserializeObject<List<Fruta>>(fruta);
-                foreach (var producto in frutas)
-                {
-                    productos.Add(producto);
+
+                    string fruta = reader.ReadToEnd();
+                    frutas = JsonConvert.DeserializeObject<List<Fruta>>(fruta);
+                    foreach (var producto in frutas)
+                    {
+                        productos.Add(producto);
+                    }
                 }
             }
 
             //HoratilzaVerdura
-            List<HortalizaVerdura> hortalizas = new List<HortalizaVerdura>();
-            using (StreamReader reader = new StreamReader(pathDirectorio + "hortalizaverdura.txt"))
+            if (File.Exists(pathDirectorio + "hortalizaverdura.txt"))
             {
-                if (!File.Exists(pathDirectorio + "hortalizaverdura.txt"))
+                List<HortalizaVerdura> hortalizas = new List<HortalizaVerdura>();
+                using (StreamReader reader = new StreamReader(pathDirectorio + "hortalizaverdura.txt"))
                 {
-                    List<Producto> productosVacios = new List<Producto>();
-                    return productosVacios;
-                }
-                string hortaliza = reader.ReadToEnd();
-                hortalizas = JsonConvert.DeserializeObject<List<HortalizaVerdura>>(hortaliza);
-                foreach (var producto in hortalizas)
-                {
-                    productos.Add(producto);
+
+                    string hortaliza = reader.ReadToEnd();
+                    hortalizas = JsonConvert.DeserializeObject<List<HortalizaVerdura>>(hortaliza);
+                    foreach (var producto in hortalizas)
+                    {
+                        productos.Add(producto);
+                    }
                 }
             }
 
             //Lacteo
-            List<Lacteo> lacteos = new List<Lacteo>();
-            using (StreamReader reader = new StreamReader(pathDirectorio + "lacteo.txt"))
+            if (File.Exists(pathDirectorio + "lacteo.txt"))
             {
-                if (!File.Exists(pathDirectorio + "lacteo.txt"))
+                List<Lacteo> lacteos = new List<Lacteo>();
+                using (StreamReader reader = new StreamReader(pathDirectorio + "lacteo.txt"))
                 {
-                    List<Producto> productosVacios = new List<Producto>();
-                    return productosVacios;
+                    if (File.Exists(pathDirectorio + "lacteo.txt"))
+                    {
+                        string lacteo = reader.ReadToEnd();
+                        lacteos = JsonConvert.DeserializeObject<List<Lacteo>>(lacteo);
+                        foreach (var producto in lacteos)
+                        {
+                            productos.Add(producto);
+                        }
+                    }
                 }
-                string lacteo = reader.ReadToEnd();
-                lacteos = JsonConvert.DeserializeObject<List<Lacteo>>(lacteo);
-                foreach (var producto in lacteos)
-                {
-                    productos.Add(producto);
-                }
+
             }
 
-            //Pescado
-            List<Pescado> pescados = new List<Pescado>();
-            using (StreamReader reader = new StreamReader(pathDirectorio + "pescado.txt"))
+            if (File.Exists(pathDirectorio + "pescado.txt"))
             {
-                if (!File.Exists(pathDirectorio + "pescado.txt"))
+                //Pescado
+                List<Pescado> pescados = new List<Pescado>();
+                using (StreamReader reader = new StreamReader(pathDirectorio + "pescado.txt"))
                 {
-                    List<Producto> productosVacios = new List<Producto>();
-                    return productosVacios;
+
+                    string pescado = reader.ReadToEnd();
+                    pescados = JsonConvert.DeserializeObject<List<Pescado>>(pescado);
+                    foreach (var producto in pescados)
+                    {
+                        productos.Add(producto);
+                    }
                 }
-                string pescado = reader.ReadToEnd();
-                pescados = JsonConvert.DeserializeObject<List<Pescado>>(pescado);
-                foreach (var producto in pescados)
-                {
-                    productos.Add(producto);
-                }
+
             }
 
             //Queso
-            List<Queso> quesos = new List<Queso>();
-            using (StreamReader reader = new StreamReader(pathDirectorio + "queso.txt"))
+            if (File.Exists(pathDirectorio + "queso.txt"))
             {
-                if (!File.Exists(pathDirectorio + "queso.txt"))
+                List<Queso> quesos = new List<Queso>();
+                using (StreamReader reader = new StreamReader(pathDirectorio + "queso.txt"))
                 {
-                    List<Producto> productosVacios = new List<Producto>();
-                    return productosVacios;
+
+                    string queso = reader.ReadToEnd();
+                    quesos = JsonConvert.DeserializeObject<List<Queso>>(queso);
+                    foreach (var producto in quesos)
+                    {
+                        productos.Add(producto);
+                    }
                 }
-                string queso = reader.ReadToEnd();
-                quesos = JsonConvert.DeserializeObject<List<Queso>>(queso);
-                foreach (var producto in quesos)
-                {
-                    productos.Add(producto);
-                }
+
             }
 
             return productos;
@@ -175,4 +172,3 @@ namespace Logica.Logicas
     }
 }
 
-    
